11b1cce21ac5a59756c4fe97dd499efa5051b2c7
Gordon <Gordoncheung00@gmail.com>
2025-03-17 17:34:21 +0100

    Add react icons

--- src/components/ToDo/ToDoItem.jsx
+++ src/components/ToDo/ToDoItem.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { RiDeleteBin6Fill } from "react-icons/ri";
 
 const ToDoItem = () => {
-  return <div className="flex flex-row p-5 bg-white rounded-md">ToDoItem</div>;
+  return (
+    <div className="flex flex-row p-5 bg-white rounded-md">
+      <div>
+        Ins Fitnessstudio gehen <br /> 12.23.21
+      </div>
+      <div>
+        <RiDeleteBin6Fill />
+      </div>
+    </div>
+  );
 };
 
 export default ToDoItem;
